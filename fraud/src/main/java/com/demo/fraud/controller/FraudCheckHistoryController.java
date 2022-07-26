62d77ce3b0cd6f990c2ae9d7283cf7d3d1b418a2
yusanghuck <g01099387636@gmail.com>
2022-07-27 01:59:04 +0900

    Add Microservices HTTP Communication In Action

--- fraud/src/main/java/com/demo/fraud/controller/FraudCheckHistoryController.java
+++ fraud/src/main/java/com/demo/fraud/controller/FraudCheckHistoryController.java
@@ -3,6 +3,7 @@ package com.demo.fraud.controller;
 import com.demo.fraud.domain.FraudCheckResponse;
 import com.demo.fraud.service.FraudCheckHistoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public class FraudCheckHistoryController {
     private final FraudCheckHistoryService fraudCheckHistoryService;
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudSter(@PathVariable Long customerId) {
         boolean isFraudlentCustomer = fraudCheckHistoryService.isFraudlentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
 
         return new FraudCheckResponse(isFraudlentCustomer);
     }
