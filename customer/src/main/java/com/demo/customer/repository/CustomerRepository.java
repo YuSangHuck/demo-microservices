1e5b5025e8849489692c37c23181198a09457b84
yusanghuck <g01099387636@gmail.com>
2022-07-27 01:21:46 +0900

    Add Database Setup to Fraud

--- customer/src/main/java/com/demo/customer/repository/CustomerRepository.java
+++ customer/src/main/java/com/demo/customer/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.demo.customer.repository;
 import com.demo.customer.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
 }
